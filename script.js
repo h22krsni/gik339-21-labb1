f5876fbc0c34a6c04757c83168e9841ff34052d0
h22krsni <h22krsni@du.se>
2023-12-04 18:31:16 +0100

    html
    små fix att det fungerar

--- script.js
+++ script.js
@@ -7,33 +7,33 @@ const TaBortKnapp = document.getElementById('TaBortKnapp');
 
 
 
-//Uppgift 5 - Skapa en fördefinierad funktion
-// Fördefinierad funktion som eventlyssnare för textfälten
+//Uppgift 5
+
 function handleEvent(e) {
-    // Lagrar name-attributet i en variabel
     const inputName = e.target.name;
     console.log(e.target);
 
     if (inputName === 'content') {
-        divruta.innerHTML = e.target.value; // Använder innerHTML för att uppdatera 'output'
+        divruta.innerHTML = e.target.value; 
     }
 }
 
 
-// Uppgift 6 - Koppla eventlyssnare
-// Koppla eventlyssnare till varje textfält för 'input' eventet
+
+
+// Uppgift 6
 for (let field of textfields) {
     field.addEventListener('input', handleEvent); 
 }
 
-// Uppdatera bakgrundsfärgen när kryssrutan ändras
+
 checkbox.addEventListener('change', function() {
-    // Hämta färginputfältet från samlingen textfields
+    
     const colorbox = textfields[0];
     if (checkbox.checked) {
         divruta.style.backgroundColor = colorbox.value;
     } else {
-        divruta.style.backgroundColor = ''; // Återställ bakgrundsfärgen om avmarkerad
+        divruta.style.backgroundColor = '';
     }
 });
 
